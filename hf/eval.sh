b893f70c670e1e926e5f78e3c16eba69119df8cc
0x71d3 <t-ide@toki.waseda.jp>
2020-09-14 17:56:27 +0900

    Make eval directory

--- hf/eval.sh
+++ hf/eval.sh
@@ -3,12 +3,12 @@ do
     echo -e "task: $TASK\n"
     for METHOD in nist bleu meteor
     do
-        python $METHOD.py data/dd_dial_ne/test.target bart/pred/$TASK.txt
+        python eval/$METHOD.py data/dd_dial_ne/test.target bart/pred/$TASK.txt
         echo
     done
     for METHOD in ent dist avg_len
     do
-        python $METHOD.py bart/pred/$TASK.txt
+        python eval/$METHOD.py bart/pred/$TASK.txt
         echo
     done
 done
