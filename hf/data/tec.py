e6af0f5dc0578c10422dc4b24dd83ff2ab503392
0x71d3 <t-ide@toki.waseda.jp>
2020-10-12 17:25:47 +0900

    Update TEC codes

--- hf/data/tec.py
+++ hf/data/tec.py
@@ -2,6 +2,11 @@ import os
 import re
 import sys
 
+emotions = ['anger', 'disgust', 'fear', 'joy', 'sadness', 'surprise']
+
+mention = re.compile(r'^@\w+\s+', re.ASCII)
+hashtag = re.compile(r'\s+#\w+$', re.ASCII)
+
 in_path = sys.argv[1]
 out_path = os.path.join(sys.argv[2], '{}.{}')
 
@@ -11,12 +16,12 @@ with open(in_path) as f:
         text, emo = line.split('\t', maxsplit=1)[1].rsplit('\t', maxsplit=1)
 
         # clean text
-        while re.search(r'^@\w+\s+', text):
-            text = re.sub(r'^@\w+\s+', '', text)
-        while re.search(r'\s+#\w+$', text):
-            text = re.sub(r'\s+#\w+$', '', text)
+        while re.search(mention, text):
+            text = re.sub(mention, '', text)
+        while re.search(hashtag, text):
+            text = re.sub(hashtag, '', text)
 
-        pairs.append((text, emo[3:].strip()))
+        pairs.append((text, emotions.index(emo[3:].rstrip())))
 
 val_size = len(pairs) // 10
 train_size = len(pairs) - 2 * val_size
@@ -31,4 +36,4 @@ for i, split in enumerate(['train', 'val', 'test']):
     for j, lang in enumerate(['source', 'target']):
         with open(out_path.format(split, lang), 'w') as f:
             for k in range(len(list_of_pairs[i])):
-                f.write(list_of_pairs[i][k][j] + '\n')
+                f.write(str(list_of_pairs[i][k][j]) + '\n')
